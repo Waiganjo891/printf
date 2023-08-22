9b1d3babddfaf3b70162c8061f8c23b936f9fa29
Waiganjo <waiganjothiongo12@gmail.com>
2023-08-22 01:39:53 -0700

    Task 1

--- flags.c
+++ flags.c
@@ -9,15 +9,15 @@ int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int b, curr_i;
+	int b, curr_a;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_a = *i + 1; format[curr_a] != '\0'; curr_a++)
 	{
 		for (b = 0; FLAGS_CH[b] != '\0'; b++)
-			if (format[curr_i] == FLAGS_CH[b])
+			if (format[curr_a] == FLAGS_CH[b])
 			{
 				flags |= FLAGS_ARR[b];
 				break;
@@ -27,7 +27,7 @@ int get_flags(const char *format, int *i)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = curr_a - 1;
 
 	return (flags);
 }
