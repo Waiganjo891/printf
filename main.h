1443dd66b98e441cb2b002dc5ff6a0fad9c300c1
Waiganjo <waiganjothiongo12@gmail.com>
2023-08-22 12:32:03 -0700

    Finish

--- main.h
+++ main.h
@@ -90,7 +90,7 @@ int write_pointer(char buffer[], int ind, int length,
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
-/****************** UTILS ******************/
+
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
